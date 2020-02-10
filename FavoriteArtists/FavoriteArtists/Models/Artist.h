19ab77df5400cff7036bac0f2aab665dadbb3349
Fabiola <saga.fabiola@gmail.com>
2020-02-10 11:23:24 -0600

    added Artist class, removed NS_ASSUME_NONNULL_BEGIN NS_ASSUME_NONNULL_END from Artist header file

---
+++ FavoriteArtists/FavoriteArtists/Models/Artist.h
@@ -0,0 +1,17 @@
+//
+//  Artist.h
+//  FavoriteArtists
+//
+//  Created by Fabiola S on 2/10/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Artist : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/Models/Artist.m
@@ -0,0 +1,13 @@
+//
+//  Artist.m
+//  FavoriteArtists
+//
+//  Created by Fabiola S on 2/10/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+#import "Artist.h"
+
+@implementation Artist
+
+@end
